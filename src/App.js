116dbde5322d4ba76c442cb247fa2247cc7851ec
David Baliles <dbaliles@heroku.com>
2018-03-06 17:47:50 -0500

    Logo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <div className = "jumbotron jumbotron-fluid">
           <div className = "container">
           
-          <h2 className = "logo"></h2><h2 className = "display-4">Private Blockchain</h2>
+          <h2 className = "display-4">ACME & Co. Private Blockchain</h2>
             
             {web3.isConnected() && <p className = "lead">Smart Contract Administration</p>}
 
